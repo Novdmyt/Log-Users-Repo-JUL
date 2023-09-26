63182901d2ad80228adb5e27ea3e77fdbe921eb7
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-26 23:15:27 +0300

    Add method createUser

---
+++ src/main/java/org/example/app/repositories/UserCreateRepository.java
@@ -0,0 +1,29 @@
+package org.example.app.repositories;
+
+import org.example.app.database.DBConnect;
+import org.example.app.entities.User;
+import org.example.app.utils.Constants;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class UserCreateRepository {
+    private static final Logger LOGGER =
+            Logger.getLogger(UserCreateRepository
+                    .class.getName());
+
+    public String createUser(User user) {
+        String sql = "INSERT INTO " + Constants.TABLE_USERS + "(name, phone, email) VALUES(?, ?, ?)";
+        try (PreparedStatement pstmt = DBConnect.connection().prepareStatement(sql)) {
+            pstmt.setString(1, user.getName());
+            pstmt.setString(2, user.getPhone());
+            pstmt.executeUpdate();
+            return Constants.DATA_INSERT_MSG;
+        } catch (SQLException e) {
+            LOGGER.log(Level.WARNING, Constants.LOG_DB_ERROR_MSG);
+            return e.getMessage();
+        }
+    }
+}
