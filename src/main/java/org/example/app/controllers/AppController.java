e6ef8b525a5a8d772cafdadd3e76aa4ac6d330d5
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-28 23:05:33 +0300

    Add code service.updateUserEmail(); in method filterChoice

--- src/main/java/org/example/app/controllers/AppController.java
+++ src/main/java/org/example/app/controllers/AppController.java
@@ -22,7 +22,8 @@ public class AppController {
             case 1 -> service.createUser();
             case 2 -> service.readUsers();
             case 3 -> service.updateUser();
-            case 4 -> service.deleteUser();
+            case 4 -> service.updateUserEmail();
+            case 5 -> service.deleteUser();
             case 0 -> view.getOutput(choice, Constants.APP_CLOSE_MSG);
             default -> service.getNoSuchOption(choice);
         }
