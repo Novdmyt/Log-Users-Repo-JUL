49ab30e557411f33728142ff305e59498ea0711d
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-09-28 23:10:14 +0300

    Fix code

--- src/main/java/org/example/app/utils/NameValidator.java
+++
@@ -1,7 +0,0 @@
-package org.example.app.utils;
-
-public class NameValidator {
-    public static boolean isNameValidator(String name){
-        return name.isEmpty()|| !name.matches(Constants.NAME_RGX);
-    }
-}
